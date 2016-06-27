4f84e894b7aa32bf8e48fe31e05dfb269b577fb1
Bermuda_UT <hoso1312@gmail.com>
2016-06-27 11:26:47 +1000

    ScriptUpdate

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    LifeSource lifeSource;
+
+	// Use this for initialization
+	void Start () {
+        lifeSource = new LifeSource();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
