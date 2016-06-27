20c1351987677e29a6b6116eb493a9a1cc1e5dbd
Harry Zhang <harryzhanga@gmail.com>
2016-06-27 12:30:20 +1000

    creature update

---
+++ Assets/Scripts/CreatureBase.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatureBase : MonoBehaviour
+{
+    public float time;
+    public bool canFly;
+    public bool canSwim;
+    public float movementSpeed;
+    public bool isHostile;
+    public bool isAggressive;
+    private LifeSource lifesource;
+    public int awarenessLevel;
+    private BoxCollider awarenessBox;
+    private float previousAwarenessLevel;
+    public float HungerRate;
+    public float Hunger;
+    void Start()
+    {
+        awarenessBox = GetComponent<BoxCollider>();
+        awarenessBox.center = transform.position;
+        awarenessBox.size = new Vector3(awarenessLevel, awarenessLevel,awarenessLevel);
+    }
+    void Update()
+    {
+        Hunger = lifesource.hunger
+    }
+    void ModifyAwareness(float NewAwarenessLevel)
+    {
+        awarenessBox.size = new Vector3(NewAwarenessLevel, NewAwarenessLevel, NewAwarenessLevel);
+    }
+    void OnTriggerStay(Collider other)
+    {
+        Collider[] colliders = Physics.OverlapBox(transform.position, awarenessBox.size);
+ 
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject.tag == "Player"){
+            }
+            
+        }
+    }
+  
+}
