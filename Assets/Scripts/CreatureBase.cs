ef638f4c077e88937076ceb7f7d75e08f97f71a1
Harry Zhang <harryzhanga@gmail.com>
2016-06-27 12:45:26 +1000

    Creature update

--- Assets/Scripts/CreatureBase.cs
+++ Assets/Scripts/CreatureBase.cs
@@ -15,16 +15,18 @@ public class CreatureBase : MonoBehaviour
     private float previousAwarenessLevel;
     public float HungerRate;
     public float CreatureHunger;
+    public float FindFoodHunger;
+    private Vector3 moveVelocity;
     void Start()
     {
-        CreatureHunger = 
+        // set creature hunger here
         awarenessBox = GetComponent<BoxCollider>();
         awarenessBox.center = transform.position;
         awarenessBox.size = new Vector3(awarenessLevel, awarenessLevel,awarenessLevel);
     }
     void Update()
     {
-        
+        CreatureHunger -= HungerRate * Time.deltaTime * time;
     }
     void ModifyAwareness(float NewAwarenessLevel)
     {
@@ -38,7 +40,11 @@ public class CreatureBase : MonoBehaviour
         {
             if (colliders[i].gameObject.tag == "Player"){
             }
-            
+            if (colliders[i].gameObject.tag == "Food" && CreatureHunger < FindFoodHunger)
+            {
+                Vector3 targetPosition = colliders[i].transform.position;
+                Vector3.SmoothDamp(transform.position, targetPosition, ref moveVelocity, 3);
+            }
         }
     }
   
